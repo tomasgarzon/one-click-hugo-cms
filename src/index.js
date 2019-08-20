e82e37ebcd527c345f958d986eaf3135c3c0eeb8
Tomas <tomas@openexo.com>
2019-08-20 17:12:27 +0200

    joinURL

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+$(function(){
+    $('#form-join').submit(function(event){
+        event.preventDefault();
+        var $this = $(this);
+        debugger;
+        console.log($this);
+    });
+})
